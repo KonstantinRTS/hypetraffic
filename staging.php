eb9c6b8af662453d2147eb736b895cdb8beb2a3c
KonstantinRTS <k.developer@acceptance.ltd>
2025-04-04 15:15:07 +0300

    all files 05

---
+++ staging.php
@@ -0,0 +1,9 @@
+<section class="home-scale">
+    <div class="home-scale__container _container">
+        <div class="home-scale__body">
+            <h2 class="home-scale__title">Scale Smarter, Not Just Louder</h2>
+            <p class="home-scale__text">Your traffic is just the beginning. Let’s turn clicks into customers and customers into revenue.</p>
+            <a href="#" class="home-scale__link">Schedule a Free Strategy Session Today</a>
+        </div>
+    </div>
+</section>

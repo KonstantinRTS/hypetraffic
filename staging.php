8a52c74ad8bfb7031e2be714f1fd88b8fd9f8a6e
KonstantinRTS <k.developer@acceptance.ltd>
2025-04-07 18:21:34 +0300

    all files 07

--- staging.php
+++ staging.php
@@ -1,24 +1,28 @@
-<section class="home-reviews">
-    <div class="home-reviews__container _container">
-        <div class="home-reviews__body">
-            <h2 class="home-reviews__label">Brands That Trust Hypetraffic</h2>
-            <h3 class="home-reviews__title">Testimonials</h3>
-            <p class="home-reviews__text">Why do clients stick with us long-term? Because we deliver measurable, repeatable, and scalable results. From explosive traffic gains to unmatched return on ad spend, we let the numbers speak</p>
-            <div class="home-reviews__items">
-                <div class="home-reviews__item">
-                    <h3 class="home-reviews__quote">“Clear, communicative, and proactive. Best SEO ROI we’ve ever seen.”</h3>
-                    <p class="home-reviews__name">Sarah G.,</p>
-                    <p class="home-reviews__job">eCommerce Manager</p>
+<section class="contacts-main">
+    <div class="contacts-main__container _container">
+        <div class="contacts-main__body">
+            <h2 class="contacts-main__title">Need Expert Assistance or Have Questions?</h2>
+            <p class="contacts-main__text">
+                Whether you’re ready to boost your performance with ROI-driven SEO & PPC or just want to know how Hypetraffic can fit into your growth plan — we’re here for it. No bots, no generic replies — just real answers from real experts. We’re here to help.
+                <br>
+                <br>
+                If you have any inquiries, reach out to our team. We’ll respond promptly and can arrange a discovery call if necessary.
+            </p>
+            <div class="contacts-main__items">
+                <div class="contacts-main__item">
+                    <h3 class="contacts-main__label">General Enquiries</h3>
+                    <a href="mailto:contact@hypetraffic.io" class="contacts-main__link">contact@hypetraffic.io</a>
                 </div>
-                <div class="home-reviews__item">
-                    <h3 class="home-reviews__quote">“We went from 3K to 12K organic visits monthly — in less than 90 days. Hypetraffic gets it.”</h3>
-                    <p class="home-reviews__name">Liam T.,</p>
-                    <p class="home-reviews__job">SaaS Founder</p>
+                <div class="contacts-main__item">
+                    <h3 class="contacts-main__label">Commercial / Partner Enquiries</h3>
+                    <a href="mailto:business@hypetraffic.io" class="contacts-main__link">business@hypetraffic.io</a>
                 </div>
-                <div class="home-reviews__item">
-                    <h3 class="home-reviews__quote">“PPC that actually pays off? Hypetraffic proved it. Our ROAS went from 1.9x to 5.3x.”</h3>
-                    <p class="home-reviews__name">Emma D.,</p>
-                    <p class="home-reviews__job">Marketing Director</p>
+                <div class="contacts-main__item">
+                    <h3 class="contacts-main__label">Our Address</h3>
+                    <div class="contacts-main__address">address example</div>
+                </div>
+                <div class="contacts-main__item">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d381428.9842513585!2d-0.4312316281021684!3d51.52817979531493!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47d8a00baf21de75%3A0x52963a5addd52a99!2z0JvQvtC90LTQvtC9LCDQktC10LvQuNC60L7QsdGA0LjRgtCw0L3QuNGP!5e1!3m2!1sru!2sua!4v1743939976225!5m2!1sru!2sua" width="100%" height="100%" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
             </div>
         </div>
